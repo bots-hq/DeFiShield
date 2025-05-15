10cba1a269801cf273969065af9c2e8c7f43353c
Salick <61610355+saalick@users.noreply.github.com>
2025-05-15 20:16:58 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from telegram.ext import Updater, MessageHandler, Filters
+from config import TELEGRAM_BOT_TOKEN
+from handlers import handle_message
+
+def main():
+    updater = Updater(TELEGRAM_BOT_TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
